e57d5e510a47a6aabcd6725da7a24249c3e79136
zzakerr <roma.ri4cov@mail.ru>
2024-11-11 16:45:14 +0300

    Back Sprint + Invisible Dor collider

---
+++ Assets/Scripts/SceneState/Saver.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+using UnityEngine;
+
+[Serializable]
+public  class Saver<T>
+{ 
+    public static void TryLoad(string fileName, ref T data)
+    {
+        var path = FileHandler.Path(fileName);
+        if (File.Exists(path))
+        {
+            Debug.Log($"Load file from :{FileHandler.Path(fileName)}");
+            var dataString = File.ReadAllText(path);
+            var saver = JsonUtility.FromJson<Saver<T>>(dataString);
+            data = saver.data;
+        }
+        else
+        {
+            Debug.Log($"The file was not found :{FileHandler.Path(fileName)}");
+        }
+    }
+
+    public static void Save(string fileName, T data)
+    {
+        Debug.Log($"File saving to :{FileHandler.Path(fileName)}");
+        var wrapper = new Saver<T>{data = data};
+        var dataString = JsonUtility.ToJson(wrapper);
+        File.WriteAllText(FileHandler.Path(fileName), dataString);
+    } 
+
+    public T data;
+}
+
+public static class FileHandler
+{
+    public static string Path(string fileName)
+    {
+        return $"{Application.persistentDataPath}/{fileName}";
+    }
+
+    public static void Reset(string fileName)
+    {
+        var path = Path(fileName);
+
+        if (File.Exists(path))
+        {
+            File.Delete(path);
+        }
+    }
+
+    internal static bool HasFile(string fileName)
+    {
+        return File.Exists(Path(fileName));
+    }
+}
